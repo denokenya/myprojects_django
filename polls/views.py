121396da417b9b894b002451dbd0fac00e076f51
denokenya <dennismuteti25@gmail.com>
2022-02-25 13:51:33 +0300

    code refactor

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,21 @@
 import imp
 from django.http import HttpResponse
+from matplotlib.style import context
 
 from .models import Question
+
+from django.template import loader
 
 
 def index(request):
 
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
 
-    return HttpResponse(output)
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
