896366c62375d1c0628dfac494f40f0ed04f8b22
denokenya <dennismuteti25@gmail.com>
2022-02-25 12:38:07 +0300

    code refactor

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 
 
+def index(request):
+
+    return HttpResponse("Hello, world . You are at the polls index.")
+
+
 def detail(request, question_id):
 
     return HttpResponse("You're looking at question %s ." % question_id)
