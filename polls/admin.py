9358dfb3fa17e8ec5929b026da330b6bdeb067a6
denokenya <dennismuteti25@gmail.com>
2022-02-28 12:05:54 +0300

    code refactor

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,13 @@
+from tkinter.messagebox import QUESTION
 from django.contrib import admin
 from .models import Question
 from .models import Choice
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
